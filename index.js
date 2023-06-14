248d96b926684663bdf98a6e3e5f25f6f8db5ba5
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2023-06-14 22:07:52 +0200

    #2 First Observable

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-import { of } from 'rxjs';
+// import { of } from 'rxjs';
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-of('Hello', 'RxJS').subscribe(console.log);
+// /*
+//  * Any code samples you want to play with can go in this file.
+//  * Updates will trigger a live reload on http://localhost:1234/
+//  * after running npm start.
+//  */
+// of('Hello', 'RxJS').subscribe(console.log);
+
+import { Observable } from "rxjs";
+
+const observer = {
+	next: (value) => console.log("next", value),
+	error: (value) => console.log("error", value),
+	complete: () => console.log("complete!"),
+};
+
+const observable = new Observable((subscriber) => {
+	subscriber.next("Hello");
+	subscriber.next("World");
+	subscriber.complete();
+});
+
+observable.subscribe(observer);
