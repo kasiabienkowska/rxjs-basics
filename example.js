1e523903b93421929e5950c4709e886bb1549494
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2023-06-19 20:46:50 +0200

    #7 Using from operator

--- example.js
+++ example.js
@@ -1,4 +1,14 @@
-import { range, of } from "rxjs";
+import { from } from "rxjs";
+
+function* hello() {
+	yield "Hello";
+	yield "World";
+}
+
+const iterator = hello();
+
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
 
 const observer = {
 	next: (val) => console.log("next", val),
@@ -7,6 +17,10 @@ const observer = {
 };
 
 // const source$ = range(1, 5);
-const source$ = of(1, 2, 3, 4, 5);
+// const source$ = from([1, 2, 3, 4, 5]);
+
+// const source$ = from(fetch("https://api.github.com/users/octocat"));
+
+const source$ = from(iterator);
 
 source$.subscribe(observer);
