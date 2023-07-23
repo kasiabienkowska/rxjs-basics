e02c9db811068debb03cdd876a1d626af104e769
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2023-07-23 21:25:39 +0200

    #Creating a scrolling bar using fromEvent and map

---
+++ scrollbar.js
@@ -0,0 +1,25 @@
+import { fromEvent } from "rxjs";
+
+import { map } from "rxjs/operators";
+
+//helpers
+function calculateScrollPercent(element) {
+	const { scrollTop, scrollHeight, clientHeight } = element;
+
+	return (scrollTop / (scrollHeight - clientHeight)) * 100;
+}
+
+//elems
+const progressBar = document.querySelector(".progress-bar");
+
+//streams
+
+const scroll$ = fromEvent(document, "scroll");
+const progress$ = scroll$.pipe(
+	//percent progress
+	map(({ target }) => calculateScrollPercent(target.scrollingElement))
+);
+
+progress$.subscribe((percent) => {
+	progressBar.style.width = `${percent}%`;
+});
